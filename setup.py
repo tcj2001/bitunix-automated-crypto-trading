b3dbd020e47b00b3dca72c208efb8027f882f7b2
DESKTOP-GBANT4V <DESKTOP-GBANT4V@server.com>
2025-04-03 12:55:58 -0500

    modified setup.py for installation

--- setup.py
+++ setup.py
@@ -1,7 +1,42 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools.command.install import install
+import sys
+import os
+import urllib.request
+import subprocess
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+
+class CustomInstall(install):
+    """Custom install class to download and install platform-specific wheels."""
+    def run(self):
+        # Detect the operating system
+        if sys.platform.startswith('linux'):
+            print("Linux detected. Downloading TA-Lib .deb package...")
+            url = "https://github.com/ta-lib/ta-lib/releases/download/v0.6.4/ta-lib_0.6.4_amd64.deb"
+            file_name = "ta-lib_0.6.4_amd64.deb"
+            urllib.request.urlretrieve(url, file_name)
+            
+            # Install the .deb package
+            subprocess.check_call(["sudo", "dpkg", "-i", file_name])
+
+        elif sys.platform == "win32":
+            print("Windows detected. Downloading TA-Lib .whl file...")
+            url = "https://github.com/cgohlke/talib-build/releases/download/v0.6.3/ta_lib-0.6.3-cp313-cp313-win_amd64.whl"
+            file_name = "ta_lib-0.6.3-cp313-cp313-win_amd64.whl"
+            urllib.request.urlretrieve(url, file_name)
+            
+            # Install the .whl file
+            subprocess.check_call([sys.executable, "-m", "pip", "install", file_name])
+
+        else:
+            print("Unsupported platform. Please manually install TA-Lib.")
+
+        # Continue with the standard install process
+        install.run(self)
+
+
 
 setup(
     name="bitunix-automated-crypto-trading",
@@ -37,13 +72,15 @@ setup(
         "colorama",
         "pandas",
         "numpy",
-        # Note: TA-LIB is commented out as it requires special installation
-        # "TA-LIB",
+        "TA-Lib",  # Technical Analysis Library
     ],
     entry_points={
         "console_scripts": [
-            "bitunix=bitunix:main",
+            "bitunix-automated-crypto-trading=bitunix-automated-crypto-trading.bitunix:main",
         ],
+    },
+    cmdclass={
+        'install': CustomInstall,  # Override the install command
     },
     package_data={
         "": ["templates/*.html", "static/*", "config.txt", "sampleenv.txt"],
