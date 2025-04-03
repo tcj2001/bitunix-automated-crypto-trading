365524bfd0c85fa4f789179b43994e10e8a1ef4a
DESKTOP-GBANT4V <DESKTOP-GBANT4V@server.com>
2025-04-03 14:14:09 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 import sys
-import os
 import urllib.request
 import subprocess
 
@@ -12,26 +11,28 @@ class CustomInstall(install):
     """Custom install class to download and install platform-specific wheels."""
     def run(self):
         # Detect the operating system
-        if sys.platform.startswith('linux'):
+        platform = sys.platform
+        if platform.startswith('linux'):
             print("Linux detected. Downloading TA-Lib .deb package...")
             url = "https://github.com/ta-lib/ta-lib/releases/download/v0.6.4/ta-lib_0.6.4_amd64.deb"
             file_name = "ta-lib_0.6.4_amd64.deb"
             urllib.request.urlretrieve(url, file_name)
-            
+
             # Install the .deb package
             subprocess.check_call(["sudo", "dpkg", "-i", file_name])
 
-        elif sys.platform == "win32":
+        elif platform == "win32":
             print("Windows detected. Downloading TA-Lib .whl file...")
             url = "https://github.com/cgohlke/talib-build/releases/download/v0.6.3/ta_lib-0.6.3-cp313-cp313-win_amd64.whl"
             file_name = "ta_lib-0.6.3-cp313-cp313-win_amd64.whl"
             urllib.request.urlretrieve(url, file_name)
-            
+
             # Install the .whl file
             subprocess.check_call([sys.executable, "-m", "pip", "install", file_name])
 
         else:
-            print("Unsupported platform. Please manually install TA-Lib.")
+            # For macOS or other platforms
+            print(f"Unsupported platform: {platform}. Please manually install TA-Lib.")
 
         # Continue with the standard install process
         install.run(self)
@@ -47,14 +48,15 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/tcj2001/bitunix-automated-crypto-trading",
-    packages=find_packages(),
+    packages=find_packages(include=['src', 'src.*']),
+    package_dir={'': '.'},
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.13",
+    python_requires=">=3.13",  # Required for the TA-Lib wheel compatibility
     install_requires=[
         "fastapi",
         "uvicorn",
@@ -76,7 +78,7 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "bitunix-automated-crypto-trading=bitunix_automated_crypto_trading.bitunix:main",
+            "bitunixautotrade=src.bitunix:main",
         ],
     },
     cmdclass={
--- src/bitunix.py
+++ src/bitunix.py
@@ -1,4 +1,4 @@
-import asyncio 
+import asyncio
 import os
 import uvicorn
 import numpy as np
@@ -48,29 +48,29 @@ settings = Settings()
 class bitunix():
     def __init__(self, password, api_key, secret_key, settings):
         self.screen_refresh_interval =settings.SCREEN_REFRESH_INTERVAL
-        
+
         self.autoTrade=settings.AUTOTRADE
-        
+
         self.threadManager = ThreadManager()
         self.notifications = NotificationManager()
         self.bitunixApi = BitunixApi(api_key, secret_key, settings)
         self.bitunixSignal = BitunixSignal(api_key, secret_key, settings, self.threadManager, self.notifications, self.bitunixApi)
-        
+
         self.websocket_connections = set()
         self.DB = {"admin": {"password": password}}
-        
+
     async def update_settings(self, settings):
         self.settings = settings
         await self.bitunixSignal.update_settings(settings)
         await self.bitunixApi.update_settings(settings)
 
-    async def start(self): 
+    async def start(self):
         await asyncio.create_task(self.bitunixSignal.load_tickers())
         await asyncio.create_task(self.bitunixSignal.start_jobs())
-        
-    async def restart(self): 
+
+    async def restart(self):
        await asyncio.create_task(self.bitunixSignal.restart_jobs())
-        
+
     async def send_message_to_websocket(self,message):
         async def send_to_all():
             for ws in self.bitunixSignal.websocket_connections:
@@ -80,7 +80,7 @@ class bitunix():
     async def send_async_message_to_websocket(self,message):
             for ws in self.bitunixSignal.websocket_connections:
                 await ws.send_text(message)
-     
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.add_middleware(
@@ -101,7 +101,7 @@ async def login(data: OAuth2PasswordRequestForm = Depends()):
     start=time.time()
     username = data.username
     password = data.password
-    
+
     user = load_user(username, some_callable_object)
     if not user or password != user["password"]:
         raise InvalidCredentialsException
@@ -123,7 +123,7 @@ def load_user(username, some_callable=some_callable_object):
 async def get_private_endpoint(user=Depends(login_manager)):
     return "You are an authenticated user"
 
-@app.on_event("startup")  
+@app.on_event("startup")
 async def startup_event():
     await asyncio.create_task(get_server_states(app, settings))
     await bitunix.start()
@@ -156,18 +156,18 @@ async def set_server_states():
 
     if settings.AUTOTRADE:
         settings.OPTION_MOVING_AVERAGE = app.state.element_states['optionMovingAverage']
-        settings.notifications.add_notification(f"optionMovingAverage: {settings.OPTION_MOVING_AVERAGE}")  
+        settings.notifications.add_notification(f"optionMovingAverage: {settings.OPTION_MOVING_AVERAGE}")
 
         settings.PROFIT_AMOUNT = app.state.element_states['profitAmount']
-        settings.notifications.add_notification(f"profitAmount: {settings.PROFIT_AMOUNT}")  
+        settings.notifications.add_notification(f"profitAmount: {settings.PROFIT_AMOUNT}")
 
         settings.LOSS_AMOUNT = app.state.element_states['lossAmount']
-        settings.notifications.add_notification(f"lossAmount: {settings.LOSS_AMOUNT}")  
+        settings.notifications.add_notification(f"lossAmount: {settings.LOSS_AMOUNT}")
 
         settings.MAX_AUTO_TRADES = app.state.element_states['maxAutoTrades']
         settings.notifications.add_notification(f"maxAutoTrades: {settings.MAX_AUTO_TRADES}")
 
-    settings.notifications.add_notification(" AutoTrade activated" if settings.AUTOTRADE else "AutoTrade de-activated")  
+    settings.notifications.add_notification(" AutoTrade activated" if settings.AUTOTRADE else "AutoTrade de-activated")
 
 
 @app.post("/autotrade")
@@ -183,7 +183,7 @@ async def read_root(request: Request):
 @app.get("/main", response_class=HTMLResponse)
 async def main_page(request: Request,  user=Depends(login_manager)):
     return templates.TemplateResponse({"request": request, "user": user}, "main.html")
- 
+
 #when main page opened
 @app.websocket("/wsmain")
 async def websocket_endpoint(websocket: WebSocket):
@@ -191,33 +191,33 @@ async def websocket_endpoint(websocket: WebSocket):
 
 async def wsmain(websocket):
     query_params = websocket.query_params
-    
+
     try:
         logger.info("local main page WebSocket connection opened")
-        
+
         await websocket.accept()
         bitunix.websocket_connections.add(websocket)
 
         queue = asyncio.Queue()
-        queueTask = asyncio.create_task(send_data_queue(websocket, queue))    
+        queueTask = asyncio.create_task(send_data_queue(websocket, queue))
         while True:
             stime=time.time()
             data={}
             try:
-                
+
                 # Handle incoming ping messages
                 await asyncio.create_task(send_pong(websocket,queue))
 
                 #combined data
                 dataframes={
-                    "portfolio" : bitunix.bitunixSignal.portfoliodfStyle, 
-                    "positions" : bitunix.bitunixSignal.positiondfStyle, 
+                    "portfolio" : bitunix.bitunixSignal.portfoliodfStyle,
+                    "positions" : bitunix.bitunixSignal.positiondfStyle,
                     "orders" : bitunix.bitunixSignal.orderdfStyle,
                     "signals" : bitunix.bitunixSignal.signaldfStyle,
                     "study" : bitunix.bitunixSignal.allsignaldfStyle,
                     "positionHistory" : bitunix.bitunixSignal.positionHistorydfStyle
                 }
-                notifications=bitunix.bitunixSignal.notifications.get_notifications()          
+                notifications=bitunix.bitunixSignal.notifications.get_notifications()
 
                 utc_time = datetime.fromtimestamp(bitunix.bitunixSignal.lastAutoTradeTime, tz=pytz.UTC)
                 atctime = utc_time.astimezone(pytz.timezone('US/Central')).strftime('%Y-%m-%d %H:%M:%S')
@@ -239,19 +239,19 @@ async def wsmain(websocket):
                 logger.info("local main page WebSocket connection closed")
                 break
             except Exception as e:
-                logger.info(f"local main page websocket unexpected error1: {e}") 
-                break      
+                logger.info(f"local main page websocket unexpected error1: {e}")
+                break
 
             elapsed_time = time.time() - stime
-            
+
             if settings.VERBOSE_LOGGING:
                 logger.info(f"wsmain: elapsed time {elapsed_time}")
             time_to_wait = max(0.01, bitunix.screen_refresh_interval - elapsed_time)
-            
+
             await asyncio.sleep(time_to_wait)
-            
+
     except Exception as e:
-        logger.info(f"local main page websocket unexpected error2: {e}")       
+        logger.info(f"local main page websocket unexpected error2: {e}")
     finally:
         queueTask.cancel()
         try:
@@ -279,34 +279,34 @@ async def send_data_queue(websocket, queue):
 @app.get("/send-message/{msg}")
 async def send_message(msg: str):
     await asyncio.create_task(bitunix.send_message_to_websocket(msg))
-    return {"message": f"Sent: {msg}"}        
+    return {"message": f"Sent: {msg}"}
 
 
-@app.post("/handle_bitunix_click") 
+@app.post("/handle_bitunix_click")
 async def handle_bitunix_click(symbol: str = Form(...)):
-    # Handle the row click event here 
-    message = f"https://www.bitunix.com/contract-trade/{symbol}" 
-    return {"message": message}                                                   
+    # Handle the row click event here
+    message = f"https://www.bitunix.com/contract-trade/{symbol}"
+    return {"message": message}
 
-@app.post("/handle_order_close_click") 
+@app.post("/handle_order_close_click")
 async def handle_order_close_click(symbol: str = Form(...), orderId: str = Form(...)):
     datajs = await bitunix.bitunixApi.CancelOrder(symbol, orderId)
     bitunix.bitunixSignal.notifications.add_notification(f'closing pending order for {symbol} {datajs["msg"]}')
 
-@app.post("/handle_close_click") 
+@app.post("/handle_close_click")
 async def handle_close_click(symbol: str = Form(...), positionId: str = Form(...), qty: str = Form(...), unrealizedPNL: str = Form(...), realizedPNL: str = Form(...)):
     datajs = await bitunix.bitunixApi.FlashClose(positionId)
     pnl=float(unrealizedPNL)+float(realizedPNL)
     bitunix.bitunixSignal.notifications.add_notification(f'closing {qty} {symbol} with a profit/loss of {pnl} ({datajs["code"]} {datajs["msg"]}')
 
-@app.post("/handle_buy_click") 
+@app.post("/handle_buy_click")
 async def handle_buy_click(symbol: str = Form(...), close: str = Form(...)):
     balance = bitunix.bitunixSignal.get_portfolio_tradable_balance()
     qty= str(balance * (float(settings.ORDER_AMOUNT_PERCENTAGE) / 100) / float(close) * int(settings.LEVERAGE))
     datajs = await bitunix.bitunixApi.PlaceOrder(symbol,qty,close,'BUY')
     bitunix.bitunixSignal.notifications.add_notification(f'Buying {qty} {symbol} @ {close} ({datajs["code"]} {datajs["msg"]})')
 
-@app.post("/handle_add_click") 
+@app.post("/handle_add_click")
 async def handle_add_click(symbol: str = Form(...), close: str = Form(...)):
     row = bitunix.bitunixSignal.positiondf.loc[bitunix.bitunixSignal.positiondf['symbol'] == symbol]
     if not row.empty:
@@ -315,14 +315,14 @@ async def handle_add_click(symbol: str = Form(...), close: str = Form(...)):
         datajs = await bitunix.bitunixApi.PlaceOrder(symbol,qty,close,row['side'].values[0])
         bitunix.bitunixSignal.notifications.add_notification(f'adding {row["side"].values[0]} {qty} {symbol} @ {close} ({datajs["code"]} {datajs["msg"]})')
 
-@app.post("/handle_sell_click") 
+@app.post("/handle_sell_click")
 async def handle_sell_click(symbol: str = Form(...), close: str = Form(...)):
     balance = bitunix.bitunixSignal.get_portfolio_tradable_balance()
     qty= str(balance * (float(settings.ORDER_AMOUNT_PERCENTAGE) / 100) / float(close) * int(settings.LEVERAGE))
     datajs = await bitunix.bitunixApi.PlaceOrder(symbol,qty,close,'SELL')
     bitunix.bitunixSignal.notifications.add_notification(f'Selling {qty} {symbol} @ {close} ({datajs["code"]} {datajs["msg"]})')
 
-@app.post("/handle_reduce_click") 
+@app.post("/handle_reduce_click")
 async def handle_reduce_click(symbol: str = Form(...), positionId: str = Form(...), qty: str = Form(...),  close: str = Form(...)):
     row = bitunix.bitunixSignal.positiondf.loc[bitunix.bitunixSignal.positiondf['symbol'] == symbol]
     if not row.empty:
@@ -331,14 +331,14 @@ async def handle_reduce_click(symbol: str = Form(...), positionId: str = Form(..
         datajs = await bitunix.bitunixApi.PlaceOrder(symbol,qty,close,'BUY' if row['side'].values[0]=='SELL' else 'SELL',positionId=positionId, reduceOnly=True)
         bitunix.bitunixSignal.notifications.add_notification(f'reducing {row["side"]} {qty} {symbol} @ {close} ({datajs["code"]} {datajs["msg"]})')
 
-@app.post("/handle_charts_click") 
+@app.post("/handle_charts_click")
 async def handle_charts_click(symbol: str = Form(...)):
     chart_url = f"/charts?symbol={symbol}"
-    return JSONResponse(content={"message": chart_url})                                      
+    return JSONResponse(content={"message": chart_url})
 
 # when charts page requested (multiple period)
-@app.get("/charts", response_class=HTMLResponse) 
-async def show_charts(request: Request, symbol: str): 
+@app.get("/charts", response_class=HTMLResponse)
+async def show_charts(request: Request, symbol: str):
     return templates.TemplateResponse({"request": request, "data": symbol}, "charts.html")
 
 # when charts page opened (multiple period)
@@ -346,10 +346,10 @@ async def show_charts(request: Request, symbol: str):
 async def websocket_endpoint(websocket: WebSocket):
     await asyncio.create_task(wscharts(websocket))
 
-async def wscharts(websocket):    
+async def wscharts(websocket):
     query_params = websocket.query_params
     ticker = query_params.get("ticker")
-    
+
     try:
         logger.info("local charts page WebSocket connection opened")
 
@@ -357,7 +357,7 @@ async def wscharts(websocket):
         bitunix.websocket_connections.add(websocket)
 
         queue = asyncio.Queue()
-        queueTask = asyncio.create_task(send_data_queue(websocket, queue))    
+        queueTask = asyncio.create_task(send_data_queue(websocket, queue))
 
         while True:
             stime=time.time()
@@ -365,7 +365,7 @@ async def wscharts(websocket):
 
                 # Handle incoming ping messages
                 await asyncio.create_task(send_pong(websocket,queue))
-                
+
                 if ticker in bitunix.bitunixSignal.tickerObjects.symbols():
                     bars=settings.BARS
                     chart1m=list(bitunix.bitunixSignal.tickerObjects.get(ticker).get_interval_ticks('1m').get_data()[-bars:])
@@ -391,10 +391,10 @@ async def wscharts(websocket):
                         "macd_chart": settings.MACD_CHART,
                         "bbm_study": settings.BBM_STUDY,
                         "bbm_chart": settings.BBM_CHART,
-                        "rsi_study": settings.RSI_STUDY,    
+                        "rsi_study": settings.RSI_STUDY,
                         "rsi_chart": settings.RSI_CHART,
                     }
-                
+
                     await queue.put(json.dumps(data))
 
             except WebSocketDisconnect:
@@ -403,16 +403,16 @@ async def wscharts(websocket):
                 break
             except Exception as e:
                 logger.info(f"local charts page websocket unexpected error1: {e}")
-                break        
+                break
 
-            
+
             elapsed_time = time.time() - stime
             if settings.VERBOSE_LOGGING:
                 logger.info(f"wscharts: elapsed time {elapsed_time}")
             time_to_wait = max(0.01, bitunix.screen_refresh_interval - elapsed_time)
             await asyncio.sleep(time_to_wait)
     except Exception as e:
-        logger.info(f"local charts page websocket unexpected error2: {e}")     
+        logger.info(f"local charts page websocket unexpected error2: {e}")
     finally:
         queueTask.cancel()
         try:
@@ -421,19 +421,19 @@ async def wscharts(websocket):
             pass
 
 # when modal chart page requested (current period)
-@app.get("/chart", response_class=HTMLResponse) 
+@app.get("/chart", response_class=HTMLResponse)
 async def chart_page(request: Request):
     return templates.TemplateResponse("modal-chart.html", {"request": request})
-           
+
 # when chart page opened (current period)
 @app.websocket("/wschart")
 async def websocket_endpoint(websocket: WebSocket):
     await asyncio.create_task(wschart(websocket))
 
-async def wschart(websocket):    
+async def wschart(websocket):
     query_params = websocket.query_params
     ticker = query_params.get("ticker")
-    
+
     try:
         logger.info("local chart page WebSocket connection opened")
 
@@ -441,7 +441,7 @@ async def wschart(websocket):
         bitunix.websocket_connections.add(websocket)
 
         queue = asyncio.Queue()
-        queueTask = asyncio.create_task(send_data_queue(websocket, queue))    
+        queueTask = asyncio.create_task(send_data_queue(websocket, queue))
 
         while True:
             stime=time.time()
@@ -449,7 +449,7 @@ async def wschart(websocket):
 
                 # Handle incoming ping messages
                 await asyncio.create_task(send_pong(websocket,queue))
-                
+
                 if ticker in bitunix.bitunixSignal.tickerObjects.symbols():
                     period=settings.OPTION_MOVING_AVERAGE
                     bars=settings.BARS
@@ -468,10 +468,10 @@ async def wschart(websocket):
                         "macd_chart": settings.MACD_CHART,
                         "bbm_study": settings.BBM_STUDY,
                         "bbm_chart": settings.BBM_CHART,
-                        "rsi_study": settings.RSI_STUDY,    
+                        "rsi_study": settings.RSI_STUDY,
                         "rsi_chart": settings.RSI_CHART,
                     }
-                
+
                     await queue.put(json.dumps(data))
 
             except WebSocketDisconnect:
@@ -480,16 +480,16 @@ async def wschart(websocket):
                 break
             except Exception as e:
                 logger.info(f"local chart page websocket unexpected error1: {e}")
-                break        
+                break
 
-            
+
             elapsed_time = time.time() - stime
             if settings.VERBOSE_LOGGING:
                 logger.info(f"wschart: elapsed time {elapsed_time}")
             time_to_wait = max(0.01, bitunix.screen_refresh_interval - elapsed_time)
             await asyncio.sleep(time_to_wait)
     except Exception as e:
-        logger.info(f"local chart page websocket unexpected error2: {e}")     
+        logger.info(f"local chart page websocket unexpected error2: {e}")
     finally:
         queueTask.cancel()
         try:
@@ -562,7 +562,7 @@ async def stream_log_file(websocket: WebSocket):
             # Determine starting point: offset_from_end
             start_index = max(len(lines) - 100, 0)
             for line in lines[start_index:]:  # Yield existing lines from offset
-                await websocket.send_text(line) 
+                await websocket.send_text(line)
             # Stream new lines added to the file
             log_file.seek(0, 2)  # Go to the end of the file
             while True:
@@ -570,21 +570,24 @@ async def stream_log_file(websocket: WebSocket):
                 if not line:
                     await asyncio.sleep(0.1)  # Wait briefly if no new line is added
                     continue
-                await websocket.send_text(line) 
+                await websocket.send_text(line)
     except WebSocketDisconnect:
         print("log Client disconnected. Stopping the log stream.")
 
-  
-if __name__ == '__main__': 
-    
+
+def main():
+    global bitunix
     bitunix = bitunix(PASSWORD, API_KEY, SECRET_KEY, settings)
     bitunix.bitunixSignal.notifications.add_notification(f"Starting....................")
     import uvicorn
     if settings.VERBOSE_LOGGING:
         llevel = "debug"
     else:
-        llevel = "error"  
+        llevel = "error"
     config1 = uvicorn.Config(app, host=HOST, port=8000, log_level=llevel, reload=False)
     server = uvicorn.Server(config1)
     server.run()
+
+if __name__ == '__main__':
+    main()
 
