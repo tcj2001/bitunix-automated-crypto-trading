1bb9bd16ccc6aa21df750e2a908e050914d69e5a
DESKTOP-GBANT4V <DESKTOP-GBANT4V@server.com>
2025-04-11 12:51:29 -0500

    use mobile friendly font

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ class CustomInstall(install):
 
 setup(
     name="bitunix_automated_crypto_trading",
-    version="3.0.2",
+    version="3.0.3",
     license="MIT",
     author="tcj2001",
     author_email="thomsonmathews@hotmail.com",
