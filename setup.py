f8d4426e5bf3bbdac57624e46583cf2d249a2d19
DESKTOP-GBANT4V <DESKTOP-GBANT4V@server.com>
2025-04-14 10:21:31 -0500

    updated setup version to 3.0.5

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ class CustomInstall(install):
 
 setup(
     name="bitunix_automated_crypto_trading",
-    version="3.0.4",
+    version="3.0.5",
     license="MIT",
     author="tcj2001",
     author_email="thomsonmathews@hotmail.com",
